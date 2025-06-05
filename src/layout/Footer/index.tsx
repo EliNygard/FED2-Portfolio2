68b3e5b13db9a82f071cd174282ab71e0a8293a1
EliNygard <eli.nygard@gmail.com>
2025-06-05 11:47:42 +0200

    Adds padding on a links

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -2,13 +2,13 @@ const Footer = () => {
   return (
     <footer className="bg-theme-secondary">
       <nav className="max-w-7xl px-7 py-11 md:py-14">
-        <ul className="flex flex-col gap-11 md:flex-row md:justify-center">
+        <ul className="flex flex-col gap-8 md:flex-row md:justify-center">
           <li>
             <a
               href="https://www.linkedin.com/in/eli-nygard"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover-underline"
+              className="hover-underline pt-2.5"
             >
               Visit my <span className="uppercase">Linkedin</span>
             </a>
@@ -18,13 +18,13 @@ const Footer = () => {
               href="https://github.com/EliNygard"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover-underline"
+              className="hover-underline pt-2.5"
             >
               Visit my <span className="uppercase">GitHub</span>
             </a>
           </li>
           <li>
-            <a href="mailto:eli.nygard@gmail.com" className="hover-underline">
+            <a href="mailto:eli.nygard@gmail.com" className="hover-underline pt-2.5">
               Send me an <span className="uppercase">email</span>
             </a>
           </li>
