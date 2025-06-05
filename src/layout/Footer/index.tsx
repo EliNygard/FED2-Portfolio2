f7af214f293ba3421cab3bc87d3db48254d59d80
EliNygard <eli.nygard@gmail.com>
2025-06-05 11:42:52 +0200

    Styles hover-underline

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -3,26 +3,28 @@ const Footer = () => {
     <footer className="bg-theme-secondary">
       <nav className="max-w-7xl px-7 py-11 md:py-14">
         <ul className="flex flex-col gap-11 md:flex-row md:justify-center">
-          <li className="hover-underline">
+          <li>
             <a
               href="https://www.linkedin.com/in/eli-nygard"
               target="_blank"
               rel="noopener noreferrer"
+              className="hover-underline"
             >
               Visit my <span className="uppercase">Linkedin</span>
             </a>
           </li>
-          <li className="hover-underline">
+          <li>
             <a
               href="https://github.com/EliNygard"
               target="_blank"
               rel="noopener noreferrer"
+              className="hover-underline"
             >
               Visit my <span className="uppercase">GitHub</span>
             </a>
           </li>
-          <li className="hover-underline">
-            <a href="mailto:eli.nygard@gmail.com">
+          <li>
+            <a href="mailto:eli.nygard@gmail.com" className="hover-underline">
               Send me an <span className="uppercase">email</span>
             </a>
           </li>
