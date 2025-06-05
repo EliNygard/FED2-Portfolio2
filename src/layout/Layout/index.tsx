4db886a453318cee63b8b96273202075471c730d
EliNygard <eli.nygard@gmail.com>
2025-06-05 13:53:03 +0200

    Adds styling

--- src/layout/Layout/index.tsx
+++ src/layout/Layout/index.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
       <Header />
-      <main className="max-w-7xl mx-auto">
+      <main className="max-w-7xl mx-auto px-2">
         <Outlet></Outlet>
       </main>
       <Footer />
